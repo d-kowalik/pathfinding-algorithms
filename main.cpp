690f1e640b802a4bfe0cdbaebe1f18aa45d08f95
Damian Kowalik <dev.damian.kowalik@gmail.com>
2019-12-26 19:27:28 +0100

    Move checking directions to separate function

--- main.cpp
+++ main.cpp
@@ -49,14 +49,25 @@ void print_graph(int graph[SIZE][SIZE], bool visited[SIZE][SIZE], Point dest) {
   putchar('\n');
 }
 
+bool visited[SIZE][SIZE];
+set<Point> next_points{};
+
+bool dijkstra_check(int graph[SIZE][SIZE], int x, int y, int dist, Point dest) {
+  if (is_in_bounds(x, y) && !visited[x][y]) {
+    if (x == dest.x && y == dest.y) {
+      printf("Found! Distance: %d\n", dist + 1);
+      return true;
+    }
+    if (graph[x][y]) {
+      next_points.emplace(x, y, dist + 1);
+    }
+  }
+  return false;
+}
+
 void dijkstra(int graph[SIZE][SIZE], Point src, Point dest) {
-  bool visited[SIZE][SIZE];
   memset(visited, false, sizeof(visited));
-  set<Point> next_points{};
-  int sx = src.x;
-  int sy = src.y;
-  visited[sx][sy] = true;
-  next_points.emplace(sx, sy);
+  next_points.emplace(src);
 
   while (!next_points.empty()) {
     Point current_point = *next_points.begin();
@@ -71,45 +82,14 @@ void dijkstra(int graph[SIZE][SIZE], Point src, Point dest) {
     print_graph(graph, visited, dest);
 
     // Search right
-    if (is_in_bounds(x + 1, y) && !visited[x + 1][y]) {
-      if (x + 1 == dest.x && y == dest.y) {
-        printf("Found! Distance: %d\n", dist + 1);
-        return;
-      }
-      if (graph[x + 1][y]) {
-        next_points.emplace(x + 1, y, dist + 1);
-      }
-    }
-    // Search left
-    if (is_in_bounds(x - 1, y) && !visited[x - 1][y]) {
-      if (x + 1 == dest.x && y == dest.y) {
-        printf("Found! Distance: %d\n", dist + 1);
-        return;
-      }
-      if (graph[x - 1][y]) {
-        next_points.emplace(x - 1, y, dist + 1);
-      }
-    }
-    // Search up
-    if (is_in_bounds(x, y - 1) && !visited[x][y - 1]) {
-      if (x == dest.x && y - 1 == dest.y) {
-        printf("Found! Distance: %d\n", dist + 1);
-        return;
-      }
-      if (graph[x][y - 1]) {
-        next_points.emplace(x, y - 1, dist + 1);
-      }
-    }
-    // Search down
-    if (is_in_bounds(x, y + 1) && !visited[x][y + 1]) {
-      if (x == dest.x && y + 1 == dest.y) {
-        printf("Found! Distance: %d\n", dist + 1);
-        return;
-      }
-      if (graph[x][y + 1]) {
-        next_points.emplace(x, y + 1, dist + 1);
-      }
-    }
+    if (dijkstra_check(graph, x + 1, y, dist, dest))
+      return;
+    if (dijkstra_check(graph, x - 1, y, dist, dest))
+      return;
+    if (dijkstra_check(graph, x, y + 1, dist, dest))
+      return;
+    if (dijkstra_check(graph, x, y - 1, dist, dest))
+      return;
 
     // printf("Currently checking:\n");
     // for (const auto &point : next_points)
@@ -125,7 +105,7 @@ int main() {
       {1, 1, 1, 0, 1, 1, 1, 1}, {1, 1, 0, 1, 1, 1, 1, 1},
       {1, 0, 1, 1, 1, 1, 1, 1}, {1, 1, 1, 0, 1, 1, 1, 1},
   };
-  dijkstra(graph, {4, 0}, {6, 6});
+  dijkstra(graph, {0, 0}, {6, 6});
 
   return 0;
 }
