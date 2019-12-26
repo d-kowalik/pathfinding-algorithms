5a6656814ae326cf6c4b16a7af32794cc558fb78
Damian Kowalik <dev.damian.kowalik@gmail.com>
2019-12-26 19:51:05 +0100

    Add a function to display the path travelled

--- main.cpp
+++ main.cpp
@@ -1,11 +1,12 @@
 #include <cstdio>
 #include <cstring>
 #include <set>
+#include <stack>
 #include <utility>
 
 #define SIZE 8
 
-using std::set, std::pair;
+using std::set, std::pair, std::make_pair, std::stack;
 
 struct Point {
   Point() = default;
@@ -46,15 +47,44 @@ void print_graph(int graph[SIZE][SIZE], bool visited[SIZE][SIZE], Point dest) {
 
 bool visited[SIZE][SIZE];
 set<Point> next_points{};
+pair<int, int> previous_points[SIZE][SIZE];
 
-bool dijkstra_check(int graph[SIZE][SIZE], int x, int y, int dist, Point dest) {
+void calculate_path(Point dest, int dist) {
+  dist += 1;
+  stack<pair<int, int>> path{};
+  int x = dest.x;
+  int y = dest.y;
+  while (dist--) {
+    path.emplace(x, y);
+    auto next_pair = previous_points[x][y];
+    x = next_pair.first;
+    y = next_pair.second;
+  }
+
+  pair<int, int> node = path.top();
+  path.pop();
+  printf("(%d, %d)", node.first, node.second);
+
+  while (!path.empty()) {
+    node = path.top();
+    path.pop();
+    printf(" -> (%d, %d)", node.first, node.second);
+  }
+  putchar('\n');
+}
+
+bool dijkstra_check(int graph[SIZE][SIZE], int x, int y, int current_x,
+                    int current_y, int dist, Point dest) {
   if (is_in_bounds(x, y) && !visited[x][y]) {
     if (x == dest.x && y == dest.y) {
       printf("Found! Distance: %d\n", dist + 1);
+      previous_points[x][y] = make_pair(current_x, current_y);
+      calculate_path(dest, dist + 1);
       return true;
     }
     if (graph[x][y]) {
       next_points.emplace(x, y, dist + 1);
+      previous_points[x][y] = make_pair(current_x, current_y);
     }
   }
   return false;
@@ -63,6 +93,7 @@ bool dijkstra_check(int graph[SIZE][SIZE], int x, int y, int dist, Point dest) {
 void dijkstra(int graph[SIZE][SIZE], Point src, Point dest) {
   memset(visited, false, sizeof(visited));
   next_points.emplace(src);
+  printf("%d\n", previous_points[0][0].first);
 
   while (!next_points.empty()) {
     Point current_point = *next_points.begin();
@@ -75,16 +106,16 @@ void dijkstra(int graph[SIZE][SIZE], Point src, Point dest) {
     print_graph(graph, visited, dest);
 
     // Search right
-    if (dijkstra_check(graph, x + 1, y, dist, dest))
+    if (dijkstra_check(graph, x + 1, y, x, y, dist, dest))
       return;
     // Search left
-    if (dijkstra_check(graph, x - 1, y, dist, dest))
+    if (dijkstra_check(graph, x - 1, y, x, y, dist, dest))
       return;
     // Search down
-    if (dijkstra_check(graph, x, y + 1, dist, dest))
+    if (dijkstra_check(graph, x, y + 1, x, y, dist, dest))
       return;
     // Search up
-    if (dijkstra_check(graph, x, y - 1, dist, dest))
+    if (dijkstra_check(graph, x, y - 1, x, y, dist, dest))
       return;
   }
 }
